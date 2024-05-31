2377ad327644837fd352eea65bb2b01a5bf72cb0
Michael Slattery <michaelslatterydev@gmail.com>
2024-05-31 21:24:01 +0900

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Michael Slattery on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
